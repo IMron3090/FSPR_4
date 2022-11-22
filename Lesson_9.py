87ae3824bc3425efeb4d9ef337cac77cff8824ff
IMron4090 <programmist924@gmail.com>
2022-11-22 19:10:49 +0500

    Adds append

--- Lesson_9.py
+++ Lesson_9.py
@@ -7,10 +7,10 @@
 # a = [2,3,4]
 # print(a is b, a == b )
 
-# a = [2,3,4]
-# b = a 
-# b.append(43)  
-# print("a =",a,"b = ",b )
+a = [2,3,4]
+b = a 
+b.append(43)  
+print("a =",a,"b = ",b )
 
 # a =[2,3,4]
 # b = a.copy()
--- h21.py
+++ h21.py
@@ -1,2 +1,6 @@
 matrix =[]
 a,b, = 3,4
+a = [2,3,4]
+b = a 
+b.append(43)  
+print("a =",a,"b = ",b )
