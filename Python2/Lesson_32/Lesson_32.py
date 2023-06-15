436ca683b8a53b094e31e82542a813e0895d6334
IMron3090 <118286149+IMron3090@users.noreply.github.com>
2023-06-15 19:31:41 +0500

    adding homework 32

--- Python2/Lesson_32/Lesson_32.py
+++ Python2/Lesson_32/Lesson_32.py
@@ -1,1 +1,25 @@
-print("Hi, my name is Imran, I'm 11 yrs old ")
+"""
+
+"""
+from pypdf import PdfReader, PdfWriter, PdfMerger  
+from pathlib import Path 
+
+# Reading PDF Files With PdfReader
+pdf_path = Path().absolute() / "practice_files" /"Pride_and_Prejudice"
+print(pdf_path)
+
+# PdfReader opens loads asnd closes file on its own
+pdf_reader = PdfReader(pdf_path)
+
+# print(pdf_reader)
+# print(len(pdf_reader.pages))
+# print(pdf_reader.metadata)
+# print(pdf_reader.metadata.title)
+
+# Extracting Text From a Page
+first_page = pdf_reader.pages[0]
+print('type',type(first_page))
+print(first_page.extract_text())
+
+for page in pdf_reader.pages[:5]: # iterator
+    print(page.extract_text())
